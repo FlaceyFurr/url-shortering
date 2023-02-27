02b7ad8ee527ebc7adfb336edb1e76bb379472d6
FlaceyFurr <flaceyfurr@gmail.com>
2023-02-27 15:52:55 +0200

    Added logic to shorten link

--- src/components/shorterLink/ShorterLink.tsx
+++ src/components/shorterLink/ShorterLink.tsx
@@ -1,18 +1,78 @@
+import { useState } from "react";
 import "./style.css";
 
 const ShorterLink = () => {
+  const [longUrl, setLongUrl] = useState("");
+  const [shortUrl, setShortUrl] = useState("");
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const response = await fetch(
+      `https://api.shrtco.de/v2/shorten?url=${longUrl}`
+    );
+    const data = await response.json();
+
+    setShortUrl(data.result.short_link);
+    setLongUrl(data.result.original_link);
+
+    const button = document.getElementById("shorter-btn");
+    if (button) {
+      button.textContent = "Copy";
+      button.style.backgroundColor = "hsl(180, 66%, 49%)";
+    }
+  };
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(
+      shortUrl.replace("https://api.shrtco.de/v2/", "")
+    );
+
+    const button = document.getElementById("shorter-btn");
+    if (button) {
+      button.textContent = "Copied!";
+      button.style.backgroundColor = "hsl(257, 27%, 26%)";
+    }
+  };
+
   return (
-    <section className="url-short">
-      <input
-        type="url"
-        className="url-short__input"
-        name="url"
-        id="url"
-        placeholder="Shorten a link here..."
-        pattern="https://.*"
-        required
-      />
-      <input type="submit" className="url-short__button" value="Shorten It!" />
+    <section className="url">
+      <form className="url-short" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="url-short__input"
+          name="url"
+          id="url"
+          value={longUrl}
+          onChange={(event) => setLongUrl(event.target.value)}
+          placeholder="Shorten a link here..."
+          pattern="https://.*"
+          required
+        />
+        <button type="submit" className="url-short__button">
+          Shorten It!
+        </button>
+      </form>
+
+      {shortUrl && (
+        <div className="shorter">
+          <div className="shorter__background">
+            <p className="shorter__text-left">
+              {longUrl.length > 40 ? longUrl.slice(0, 40) + "..." : longUrl}
+            </p>
+            <div className="shorter__right">
+              <p className="shorter__text-right">{shortUrl}</p>
+              <button
+                onClick={handleCopy}
+                className="shorter__copy"
+                id="shorter-btn"
+              >
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
